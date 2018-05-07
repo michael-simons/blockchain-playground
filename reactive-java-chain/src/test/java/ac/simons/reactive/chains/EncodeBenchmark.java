d066e74e751762a380c6d27b1ecb2dbac4d6b387
Michael J. Simons <michael@simons.ac>
2018-05-07 08:23:24 +0200

    Run benchmark in throughput mode

--- reactive-java-chain/src/test/java/ac/simons/reactive/chains/EncodeBenchmark.java
+++ reactive-java-chain/src/test/java/ac/simons/reactive/chains/EncodeBenchmark.java
@@ -3,6 +3,7 @@ package ac.simons.reactive.chains;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static org.openjdk.jmh.annotations.Mode.AverageTime;
+import static org.openjdk.jmh.annotations.Mode.Throughput;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -14,7 +15,13 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-@BenchmarkMode(AverageTime)
+/**
+ * For interpreting the result, read <a href="https://blog.codecentric.de/en/2017/10/performance-measurement-with-jmh-java-microbenchmark-harness/">Performance measurement with JMH – Java Microbenchmark Harness</a>
+ * by <a href="https://twitter.com/_Atze">Kevin</a>.
+ *
+ * <blockquote>"Mode.AverageTime a lower score is preferred, while using Mode.Throughput a higher value points to better performance."</blockquote>
+ */
+@BenchmarkMode(Throughput)
 @OutputTimeUnit(NANOSECONDS)
 @Fork(3)
 public class EncodeBenchmark {
